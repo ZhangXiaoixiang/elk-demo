7e94f0448ef01d181d0429f2f32670795497b9de
zhangxiaoxiang <1090510535@qq.com>
2019-06-27 18:28:20 +0800

    参考  https://blog.csdn.net/forezp/article/details/71189836

---
+++ src/main/java/com/example/elkdemo/ElkApp.java
@@ -0,0 +1,13 @@
+package com.example.elkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElkApp.class, args);
+    }
+
+}
